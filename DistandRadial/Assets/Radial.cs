d1a2d8e215de5ad79a3877317dfe0b1ca6fde455
Andrei Quirante <andrei.quirante@ciit.edu.ph>
2024-04-25 00:59:12 +0800

    radial

--- DistandRadial/Assets/Radial.cs
+++ DistandRadial/Assets/Radial.cs
@@ -1,8 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
-#if UNITY_EDITOR
 using UnityEditor;
-#endif
 using UnityEngine;
 
 public class Radial : MonoBehaviour
@@ -11,17 +9,17 @@ public class Radial : MonoBehaviour
     Transform enemyTF;
     [Range(0f, 5f)]
     public float radius;
-    public bool isInside = false;
+    public bool isInside;
+    public float Dist;
 
     private void OnDrawGizmos()
     {
         Vector2 origin = transform.position;
 
-        if(isInside)
-        {
+        //Dist = Vector3.Distance(origin, enemyTF.position);
+        Dist = Mathf.Sqrt(origin.x * origin.x + enemyTF.position.x * enemyTF.position.x);
 
-        }
-
+        isInside = Dist <= radius;
 
         Handles.color = isInside ? Color.green : Color.red; 
         Handles.DrawWireDisc(origin, new Vector3(0, 0, 1), radius);
