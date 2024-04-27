945e080940b41259cacbbced78f7aba1b96f4228
HehNice <122663831+HehNiceArt@users.noreply.github.com>
2024-04-27 15:39:05 +0800

    Cubic Bezier

--- Bezier_Curves/Assets/Editor/BezierGizmo.cs
+++ Bezier_Curves/Assets/Editor/BezierGizmo.cs
@@ -13,12 +13,14 @@ public class BezierGizmo : Editor
         Vector3 newp0Target = Handles.PositionHandle(bezier.p0.position, Quaternion.identity);
         Vector3 newp1Target = Handles.PositionHandle(bezier.p1.position, Quaternion.identity);
         Vector3 newp2Target = Handles.PositionHandle(bezier.p2.position, Quaternion.identity);
+        Vector3 newp3Target = Handles.PositionHandle(bezier.p3.position, Quaternion.identity);
         if (EditorGUI.EndChangeCheck())
         {
             Undo.RecordObject(bezier, "Change Look At Target Postion");
             bezier.p0.position = newp0Target;
             bezier.p1.position = newp1Target;
             bezier.p2.position = newp2Target;
+            bezier.p3.position = newp3Target;
             bezier.Update();
         }
 
--- Bezier_Curves/Assets/Scripts/Bezier.cs
+++ Bezier_Curves/Assets/Scripts/Bezier.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 [ExecuteInEditMode, RequireComponent (typeof(LineRenderer))]
 public class Bezier : MonoBehaviour
 {
-    public Transform p0, p1, p2;
+    public Transform p0, p1, p2, p3;
     [Range(0,10)]
     public float Width;
     [Range(0,1)]
@@ -27,28 +27,48 @@ public class Bezier : MonoBehaviour
     {
         Handles.color = circle;
         Handles.DrawSolidDisc(p0.position, _2DSpace, _discSize);
+        Handles.Label(p0.position + Vector3.up /2, "p0");
         Handles.DrawSolidDisc(p1.position, _2DSpace, _discSize);
+        Handles.Label(p1.position + Vector3.up /2, "p1");
         Handles.DrawSolidDisc(p2.position, _2DSpace, _discSize);
+        Handles.Label(p2.position + Vector3.up /2, "p2");
+        Handles.DrawSolidDisc(p3.position, _2DSpace, _discSize);
+        Handles.Label(p3.position + Vector3.up /2, "p3");
 
         Handles.color = Color.white;
         Handles.DrawDottedLine(p0.position, p1.position, 5f);
         Handles.DrawDottedLine(p1.position, p2.position, 5f);
+        Handles.DrawDottedLine(p2.position, p3.position, 5f);
 
         Vector3 p0top1 = Linear(p0.position, p1.position);
         Vector3 p1top2 = Linear(p1.position, p2.position);
+        Vector3 p2top3 = Linear(p2.position, p3.position);
 
         Handles.color = midFill;
         Handles.DrawLine(p0.position, p0top1, Width);
         Handles.DrawLine(p1.position, p1top2, Width);
+        Handles.DrawLine(p2.position, p2top3, Width);
+
         Handles.color = midCircle;
         Handles.DrawSolidDisc(p0top1, _2DSpace, _discSize);
         Handles.DrawSolidDisc(p1top2, _2DSpace, _discSize);
+        Handles.DrawSolidDisc(p2top3, _2DSpace, _discSize);
 
         Handles.color = midConnector;
         Handles.DrawLine(p0top1, p1top2, Width);
+        Handles.DrawLine(p1top2, p2top3, Width);
 
-        Vector3 mid = Linear(p0top1, p1top2);
-        Handles.DrawSolidDisc(mid, _2DSpace, _discSize);
+        Vector3 p0top2Mid = Linear(p0top1, p1top2);
+        Vector3 p2top3Mid = Linear(p1top2, p2top3);
+        Handles.Label(p0top2Mid + Vector3.up / 2, "p0 to p2");
+        Handles.DrawSolidDisc(p0top2Mid, _2DSpace, _discSize);
+        Handles.Label(p2top3Mid + Vector3.up / 2, "p1 to p3");
+        Handles.DrawSolidDisc(p2top3Mid, _2DSpace, _discSize);
+
+        Handles.color = bezier;
+        Handles.DrawLine(p0top2Mid, p2top3Mid, Width);
+        Vector3 cubicMid = Linear(p0top2Mid, p2top3Mid);
+        Handles.DrawSolidDisc(cubicMid, _2DSpace, _discSize);
     }
    
     public virtual void Update()
@@ -63,7 +83,22 @@ public class Bezier : MonoBehaviour
         for (int i = 0; i < points; i++)
         {
             float _t = i / (points - 1.0f);
-            pos = (1.0f - _t) * (1.0f - _t) * p0.position + 2.0f * (1.0f - _t) * _t * p1.position + _t * _t * p2.position;
+            float _t2 = _t * _t;
+            float _t3 = _t * _t * _t;
+            float oneT = 1.0f - _t;
+            float oneT2 = oneT * oneT;
+            float oneT3 = oneT * oneT * oneT;
+
+            Vector3 _p0 = p0.position;
+            Vector3 _p1 = p1.position;
+            Vector3 _p2 = p2.position;
+            Vector3 _p3 = p3.position;
+            //Quadratic
+            //pos = oneT2 * _p0 + 2.0f * oneT * _t * _p1 + _t2 * _p2;
+
+            //Cubic
+            pos = oneT3 * _p0 + 3 * oneT2 * _t * _p1 + (3 * oneT * _t2) * _p2 + _t3 * _p3;
+
             line.startWidth = lineWidth;
             line.endWidth = lineWidth;
             line.SetPosition(i, pos);
